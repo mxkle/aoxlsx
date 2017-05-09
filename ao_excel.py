6bf1c3220c32e937c0381d5c004a3132dc5fb528
mxkle <maxklee@live.de>
2017-05-09 22:15:48 +0200

    Changes to be committed: 	modified:   ao_excel.py 	modified:   ao_helper.py 	modified:   ao_main.py 	modified:   excel.xlsx

--- ao_excel.py
+++ ao_excel.py
@@ -1,56 +1,35 @@
 from openpyxl import load_workbook
 from collections import OrderedDict
-import datetime
 
 def init_wb(i):
-	wb = load_workbook('excel.xlsx')
-	try:
-		ws = wb[i]
-	except:
-		ws = wb.create_sheet(i)
-		hl = ['Date','Action','Amount','Price/1','Price/total','Tier','Location']
-		for col in ws.iter_cols(min_row=1, max_col=7, max_row=1):
-			for cell in col:
-				cell.value=hl.pop(0)
-				
-	#print(ws,wb.sheetnames)
-	return (ws,wb)
-	
-def init_values(ws,args):
-	dt = datetime.datetime.today().strftime("%Y-%m-%d")
-	max = ws.max_row
-	if max == 1:
-		max+=1
-	else:
-		c = 'A'+str(max)
-			while ws[c].value == None or ws[c].value == "Date":
-			max -= 1
-			c = 'A'+str(max)
-		
-	del args['item']
-	if args['p'] == None:
-		args['p'] = args['P']/args['a']
-	if args['P'] == None:
-		args['P'] = args['p']*args['a']
-	
-	args['x'] = "buy" if args['x'] == True else "sell"
-	vals = {'date':dt}
-	vals.update({k:args[k] for k in args.keys() if args[k]})
-	
-	print(vals,max)
-	return (vals, max)
-	
-def insert_values(ws,args):
-	vals, max = init_values(ws,args)
-	if ws['A'+str(max)].value != vals['date']:
-		max += 1
-		for k in vals.keys():
-			col=1
-			d = ws.cell(row=max, column=col, value=vals[k])
-			col+=1
-		
-		
+        wb = load_workbook('excel.xlsx')
+        try:
+                ws = wb[i]
+        except:
+                ws = wb.create_sheet(i)
+                hl = ['Date','Action','Amount','Price/1','Price/total','Tier','Location']
+                for col in ws.iter_cols(min_row=1, max_col=7, max_row=1):
+                        for cell in col:
+                                cell.value=hl.pop(0)
+
+        return (ws,wb)
+
+def get_max(ws):
+        max = ws.max_row
+        c = 'A'+str(max)
+        while ws[c].value == None:
+                max -= 1
+                c = 'A'+str(max)
+
+        return(max)
+
+def insert_values(ws,vals):
+        max = get_max(ws)
+        if ws['A'+str(max)].value != vals['date']: 
+                max += 1
+                ws.append(list(vals.values()))
+
 def save_file(wb):
-    wb.save(filename = "excel.xlsx")
+        wb.save(filename = "excel.xlsx")
 
 
--- ao_main.py
+++ ao_main.py
@@ -1,17 +1,29 @@
 from ao_helper import helper
 from ao_excel import init_wb, insert_values, save_file
-import sys
+import sys,datetime
+from collections import OrderedDict
 
 def call_helper(ins):
-    #will be replaced with sys.argv call
-	#del(sys.argv[0])
-    #print(sys.argv)
-	return helper(['gold','-a','222','-b','-P','32232'])
+        #will be replaced with sys.argv call
+        #del(sys.argv[0])
+        #print(sys.argv)
+        return helper(['fibre','-t','4','-l','thetford','-a','222','-b','-P','32232'])
 
-args=call_helper(0)
-print(args)
+args = OrderedDict()
+args['date'] = datetime.datetime.today().strftime("%Y-%m-%d")
+args.update(call_helper(0))
+if args['p'] == None:
+        args['p'] = args['P']/args['a']
+if args['P'] == None:
+        args['P'] = args['p']*args['a']
 
-worksheet,workbook=init_wb(args.get('item').title())
+args['x'] = "buy" if args['x'] == True else "sell"
+#print(args,args['item'].title())
+if args['item'] == "gold":
+        del(args['t'])
+        del(args['l'])
+
+worksheet,workbook = init_wb(args['item'].title())
+del(args['item'])
 insert_values(worksheet,args)
-
 save_file(workbook)
