5fbb25c0e71dbc030a68f5ba1b5a3bce517a015f
mxkle <maxklee@live.de>
2017-05-09 16:15:32 +0200

    Updated ao_excel.py and ao_helper.py
    Adding functions to ao_excel
    Changed order in ao_helper

--- ao_helper.py
+++ ao_helper.py
@@ -2,23 +2,30 @@ import argparse
 
 towns=['caerleon','thetford','martlock','lymhurst','fort sterling']
 tier=[x for x in range(1,9)]
+
 def helper(parsed):
-    parser = argparse.ArgumentParser(description="albion market helper\nwrite down stuff you buy sell, input will get piped to excel sheet for data collection. graphs will be created and such.") 
-    parser.add_argument('item',help='item that was bought/sold')
+	parser = argparse.ArgumentParser(description="albion market helper\nwrite down stuff you buy sell, input will get piped to excel sheet for data collection. graphs will be created and such.") 
+	parser.add_argument('item',help='item that was bought/sold')
     #parser.add_argument('sheet',help='the excel sheet')
-    parser.add_argument('-t',choices=tier,help='tier of the item',metavar='1..8')
-    parser.add_argument('-a',type=int,help='amount the item that was traded',nargs=1)
-    parser.add_argument('-l',choices=towns,help='location where item was traded')
+	
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('-b',help="buy",action='store_true',dest='x')
+	group.add_argument('-s',help="sell",action='store_false',dest='x')    
+	
+	parser.add_argument('-a',type=int,help='amount the item that was traded')
+	
+	group2 = parser.add_mutually_exclusive_group(required=True)
+	group2.add_argument('-p',type=int,help='give price per item',metavar='S')
+	group2.add_argument('-P',type=int,help='give total price',metavar='S')
+	
+	parser.add_argument('-t',choices=tier,help='tier of the item',metavar='1..8')
+	parser.add_argument('-l',choices=towns,help='location where item was traded')
 
-    group2 = parser.add_mutually_exclusive_group(required=True)
-    group2.add_argument('-P',type=int,help='give total price',metavar='S',nargs=1)
-    group2.add_argument('-p',type=int,help='give price per item',metavar='S',nargs=1)
+	if len(parsed) == 0:
+		parser.print_help()
+	else:
+		args = parser.parse_args(parsed)
+		return vars(args)
 
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-b',help="buy",action='store_true')
-    group.add_argument('-s',help="sell",action='store_true')
-    if len(parsed) == 0:
-      parser.print_help()
-    else:
-      args = parser.parse_args(parsed)
-      return vars(args)
+#args=helper(['gold','-a','222','-b','-P','32232'])
+#print(args)
--- ao_main.py
+++ ao_main.py
@@ -1,12 +1,17 @@
 from ao_helper import helper
-from ao_excel import  bb
-from openpyxl import Workbook
+from ao_excel import init_wb, insert_values, save_file
 import sys
 
-def call_helper(sys.argv):
-    del(sys.argv[0])
-    print(sys.argv)
-    args=helper(['gold','-b','-p','232'])
+def call_helper(ins):
+    #will be replaced with sys.argv call
+	#del(sys.argv[0])
+    #print(sys.argv)
+	return helper(['gold','-a','222','-b','-P','32232'])
 
+args=call_helper(0)
+print(args)
 
-print(args.get('item').title())
+worksheet,workbook=init_wb(args.get('item').title())
+insert_values(worksheet,args)
+
+save_file(workbook)
